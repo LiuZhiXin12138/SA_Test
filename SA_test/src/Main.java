adcbf405611d7e5bbe22411ecfbc1c9fe2522660
LiuZhiXin12138 <105156708+LiuZhiXin12138@users.noreply.github.com>
2022-10-16 14:49:34 +0800

    Delete SA_test directory

--- SA_test/src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("*********************软件体系结构教学系统*********************");
-        mwindow m = new mwindow();
-        m.foo();
-    }
-}
